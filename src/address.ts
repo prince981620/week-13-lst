a5e3577b8bea115d734db6dfe4d195fd2aadd036
prince981620 <prince981620@gmail.com>
2024-11-02 18:54:48 +0530

    added ts logic to mint,burn token

--- src/address.ts
+++ src/address.ts
@@ -1,4 +1,4 @@
 export const PRIVATE_KEY = "";
-export const PUBLIC_KEY = "";
+export const PUBLIC_KEY = "BYq5RpPNPa4WEHJgNtapdP2Re5wtyzNSTLbcNwtdguM8";
 
 export const TOKEN_MINT_ADDRESS = "";
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,28 @@ import express from 'express';
 import { burnTokens, mintTokens, sendNativeTokens } from './mintTokens';
 
 const app = express();
-
+app.use(express.json());
+app.get('/helius',(req,res)=>{
+    console.log("working");
+    res.status(400).json({
+        msg: "working"
+    })
+})
+ 
+// "nativeTransfers": [
+//     {
+//       "amount": 1000000000,
+//       "fromUserAccount": "7MBcR9GQs94CWwL2SwzgSuhTa8guzb1dAVTDPCpnFzr9",
+//       "toUserAccount": "BYq5RpPNPa4WEHJgNtapdP2Re5wtyzNSTLbcNwtdguM8"
+//     }
 
 app.post('/helius', async(req, res) => {
-    const fromAddress = req.body.fromAddress;
-    const toAddress = req.body.toAddress;
-    const amount = req.body.amount;
+    // const body = req.body.json();
+    console.log(req.body);
+
+    const fromAddress = req.body.nativeTransfers[0].fromUserAccount;
+    const toAddress = req.body.nativeTransfers[0].toUserAccount;
+    const amount = req.body.nativeTransfers[0].amount;
     const type = "received_native_sol";
 
     if (type === "received_native_sol") {
